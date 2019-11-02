163112fa803d28fa5a27ccc25b928c42a1af7ed6
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2019-11-02 13:13:17 +0100

    Refactor according to solution

--- instructions.md
+++ instructions.md
@@ -38,3 +38,19 @@ Let's return to #build_move_tree. We'll use our #new_move_positions instance met
 To ensure that your tree represents only the shortest path to a given position, build your tree in a breadth-first manner. Take inspiration from your BFS algorithm: use a queue to process nodes in FIFO order. Start with a root node representing the start_pos and explore moves from one position at a time.
 
 Next build nodes representing positions one move away, add these to the queue. Then take the next node from the queue... until the queue is empty.
+
+## Phase III: #find_path
+
+Now that we have created our internal data structure (the move tree stored in self.root_node), we can traverse it to find the shortest path to any position on the board from our original @start_pos.
+
+Create an instance method #find_path(end_pos) to search for end_pos in the move tree. You can use either dfs or bfs search methods from the PolyTreeNode exercises; it doesn't matter. This should return the tree node instance containing end_pos.
+
+This gives us a node, but not a path. Lastly, add a method #trace_path_back to KnightPathFinder. This should trace back from the node to the root using PolyTreeNode#parent. As it goes up-and-up toward the root, it should add each value to an array. #trace_path_back should return the values in order from the start position to the end position.
+
+Use #trace_path_back to finish up #find_path.
+
+Here are some example paths that you can use for testing purposes (yours might not be exactly the same, but should be the same number of steps);
+
+kpf = KnightPathFinder.new([0, 0])
+kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
--- knightPathFinder.rb
+++ knightPathFinder.rb
@@ -2,7 +2,7 @@ require "byebug"
 require_relative "polyTreeNode"
 
 class KnightPathFinder
-  # minesweeper code
+  # minesweeper inspired-code
   attr_reader :root_node
 
   DELTAS = [
@@ -16,64 +16,13 @@ class KnightPathFinder
     [-2, 1],  # left-bottom
   ].freeze
 
-  def initialize(arr)
-    @root_node = PolyTreeNode.new(arr)
-    @considered_positions = [arr]
-  end
-
-  def find_path(target)
-    build_move_tree(@root_node)
-
-    # debugger
-    target_node = @root_node.bfs(target)
-
-    # debugger
-    trace_path_back(target_node)
-
-    # path_list
-  end
-
-  # use a queue to process nodes in FIFO order. Start with a root node representing
-  # the start_pos and explore moves from one position at a time.
-
-  # Next build nodes representing positions one move away, add these to the queue.
-  # Then take the next node from the queue... until the queue is empty.
-  def build_move_tree(startingNode)
-    # debugger
-    # queque_positions
-    # starting_node_moves behaves as Queque
-
-    queue = [startingNode]
-
-    until queue.empty?
-      node = queue.shift
-      node_moves = new_move_positions(node.value)
-
-      until node_moves.empty?
-        pos = node_moves.shift
-
-        node_child = PolyTreeNode.new(pos)
-        node.add_child(node_child)
-
-        queue.push(node_child)
-      end
-
-      # debugger
-    end
-
-    # queque_children_nodes = []
-
-  end
-
-  #   minesweeper code
-  def valid_moves(pos)
-    # debugger
-    # te nawiasy chyba zbędne, to mogą przypominać, o destrukturyzacji
+  #   static method
+  def self.valid_moves(pos)
     possible_coords = DELTAS.map do |(dx, dy)|
       [pos[0] + dx, pos[1] + dy]
     end
 
-    possible_valid_coords = possible_coords.select do |row, col|
+    possible_valid_coords = possible_coords.select do |(row, col)|
       [row, col].all? do |coord|
         coord.between?(0, 8)
       end
@@ -82,25 +31,53 @@ class KnightPathFinder
     possible_valid_coords
   end
 
-  #   it should call the ::valid_moves class method, but filter out any positions that are already in @considered_positions.
-  def new_move_positions(pos)
-    # debugger
-    new_move_positions = valid_moves(pos).reject { |move| @considered_positions.include?(move) }
-    @considered_positions.push(*new_move_positions)
+  def initialize(arr)
+    @root_node = PolyTreeNode.new(arr)
+    @considered_positions = [arr]
+  end
 
-    new_move_positions
+  def find_path(target)
+    build_move_tree
+
+    target_node = @root_node.bfs(target)
+
+    trace_path_back(target_node)
+  end
+
+  private_constant :DELTAS
+
+  private
+
+  def build_move_tree
+    queue = [@root_node]
+
+    until queue.empty?
+      node = queue.shift
+      node_moves = new_move_positions(node.value)
+
+      node_moves.each do |node_move|
+        node_child = PolyTreeNode.new(node_move)
+        node.add_child(node_child)
+
+        queue.push(node_child)
+      end
+    end
+  end
+
+  def new_move_positions(pos)
+    KnightPathFinder.valid_moves(pos)
+                    .reject { |move| @considered_positions.include?(move) }
+                    .each { |move| @considered_positions.push(move) }
   end
 
   def trace_path_back(node)
-    # debugger
     path_list = [node.value]
 
     until node.parent.nil?
-      path_list.push(node.parent.value)
+      path_list << node.parent.value
       node = node.parent
     end
 
-    # todo robić push zamiast unshift, a na końcu reverse
     path_list.reverse
   end
 end
